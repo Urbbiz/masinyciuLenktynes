fe17a53f84b3045fcb1a23022e1d2d5c0f8fb773
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-01-12 17:51:39 +0200

    first commitsss

--- src/masinyteslenktynes/MasinytesLenktynes.java
+++ src/masinyteslenktynes/MasinytesLenktynes.java
@@ -15,7 +15,7 @@ public class MasinytesLenktynes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // sdddddsdTODO code application logic here
     }
   
    
